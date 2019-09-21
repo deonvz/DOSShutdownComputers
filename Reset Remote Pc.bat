2a6effbff2088a5fbc5775b3e2d5155d78309f40
Deon van Zyl <Deon.vanzyl@mweb.co.za>
2019-09-21 13:28:34 +0200

    Initial

---
+++ Reset Remote Pc.bat
@@ -0,0 +1,8 @@
+Rem---Deon`s Dos Commands--------
+
+@Echo..This will Force a restart a Remote Machine.
+@Echo..Replace /R with /S if you want to shutdown instead of restart
+shutdown /R /m \\computername /Y
+@Echo...Command Executed !..
+Pause
+Exit
